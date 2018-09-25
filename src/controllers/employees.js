3de101ca5ee119c03b5700add7b8cb2a2800f59c
Matías Pierobón Marcos <matias.pierobonm@gmail.com>
2018-09-25 04:24:55 -0300

    add employee controller

---
+++ src/controllers/employees.js
@@ -0,0 +1,17 @@
+const Employee = require('../models/Employee');
+
+exports.all = async (args) => Employee.find(args, '-password').populate('type');
+
+exports.get = async _id => Employee.findById({ _id }, '-password').populate('type');
+
+exports.update = async (_id, args) => {
+  const employee = await Employee.findOne({ _id });
+  employee.set(args);
+  await employee.save();
+  employee.password = undefined;
+  return employee
+}
+
+exports.delete = async _id => Employee.deleteOne({ _id });
+
+exports.create = async args => Employee.create(args).select('-password');
